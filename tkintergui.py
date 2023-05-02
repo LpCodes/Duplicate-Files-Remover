6bc4359f1a230467aea883ee9021865284bef524
LP <lovelesh_p@zohomail.in>
2023-05-02 13:55:41 +0530

    ++

--- tkintergui.py
+++ tkintergui.py
@@ -42,6 +42,49 @@ def search_duplicates():
                 else:
                     file_hashes[file_hash].append(file_path)
 
+    if len(file_hashes) == 0:
+        messagebox.showinfo("No Duplicates Found", "No duplicate files were found in the selected folder.")
+        status_label.config(text="No duplicates found.")
+    else:
+        for file_hash, file_paths in file_hashes.items():
+            if len(file_paths) > 1:
+                for file_path in file_paths:
+                    file_list.insert(tk.END, file_path)
+
+        status_label.config(text=f"Found {len(file_hashes)} duplicates.")
+
+    folder_path_str = folder_path.get()
+    if not folder_path_str:
+        messagebox.showwarning("Warning", "Please select a folder to search.")
+        return
+
+    file_list.delete(0, tk.END)
+    status_label.config(text="Searching for duplicates...")
+
+    file_sizes = {}
+    file_hashes = {}
+
+    for root_folder, _, files in os.walk(folder_path_str):
+        for file in files:
+            file_path = os.path.join(root_folder, file)
+            file_size = os.path.getsize(file_path)
+
+            if file_size not in file_sizes:
+                file_sizes[file_size] = [file_path]
+            else:
+                file_sizes[file_size].append(file_path)
+
+    for file_size, file_paths in file_sizes.items():
+        if len(file_paths) > 1:
+            for file_path in file_paths:
+                with open(file_path, "rb") as f:
+                    file_hash = hashlib.md5(f.read()).hexdigest()
+
+                if file_hash not in file_hashes:
+                    file_hashes[file_hash] = [file_path]
+                else:
+                    file_hashes[file_hash].append(file_path)
+
     for file_hash, file_paths in file_hashes.items():
         if len(file_paths) > 1:
             for file_path in file_paths:
@@ -80,33 +123,52 @@ def remove_all_duplicates():
     file_list.delete(0, tk.END)
     status_label.config(text="All duplicates removed.")
 
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from tkinter import ttk
 
 root = tk.Tk()
 root.title("Duplicate File Finder")
+root.geometry("900x500")
 
-label1 = tk.Label(root, text="Select a folder to search:")
+# change the theme to 'clam'
+style = ttk.Style()
+style.theme_use('clam')
+
+# create a style for the GUI
+style.configure("TButton", font=("Helvetica", 14), foreground="white", background="#4CAF50", pady=10, padx=20)
+style.configure("TLabel", font=("Helvetica", 14), foreground="#333", pady=8)
+style.configure("TEntry", font=("Helvetica", 14), pady=8, padx=10)
+style.configure("TListbox", font=("Helvetica", 14), pady=8, padx=10)
+style.configure("TFrame", background="#f8f9fa")
+
+# create a frame to contain all the widgets
+frame = ttk.Frame(root)
+frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+
+label1 = ttk.Label(frame, text="Select a folder to search:")
 label1.pack()
 
 folder_path = tk.StringVar()
-folder_entry = tk.Entry(root, textvariable=folder_path, width=40)
+folder_entry = ttk.Entry(frame, textvariable=folder_path, width=40)
 folder_entry.pack(side=tk.LEFT)
 
-browse_button = tk.Button(root, text="Browse", command=browse_folder)
+browse_button = ttk.Button(frame, text="Browse", command=browse_folder)
 browse_button.pack(side=tk.LEFT)
 
-search_button = tk.Button(root, text="Search for Duplicates", command=search_duplicates)
+search_button = ttk.Button(frame, text="Search for Duplicates", command=search_duplicates)
 search_button.pack(pady=10)
 
-remove_button = tk.Button(root, text="Remove Selected Duplicates", command=remove_duplicates)
+remove_button = ttk.Button(frame, text="Remove Selected Duplicates", command=remove_duplicates)
 remove_button.pack(pady=10)
 
-remove_all_button = tk.Button(root, text="Remove All Duplicates", command=remove_all_duplicates)
+remove_all_button = ttk.Button(frame, text="Remove All Duplicates", command=remove_all_duplicates)
 remove_all_button.pack(pady=10)
 
-file_list = tk.Listbox(root, width=80, height=20)
+file_list = tk.Listbox(frame, width=80, height=20)
 file_list.pack()
 
-status_label = tk.Label(root, text="")
+status_label = ttk.Label(frame, text="")
 status_label.pack(pady=10)
 
 root.mainloop()
